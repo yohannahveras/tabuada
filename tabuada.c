8055a603b49cc7a1d34444f4bc9b537aa77e99d2
Yohannah Véras <yohannahlv@gmail.com>
2023-05-11 11:05:53 -0300

    first commit

---
+++ tabuada.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+  int main ()
+  {
+
+    int n, i, resultado;
+    
+    printf("Deseja a tabuada para qual valor? ");
+    scanf("%d", &n);
+    
+    for (i = 1; i <= 10; i++) {
+        resultado = n * i;
+        printf("%d x %d = %d\n", n, i, resultado);
+    }
+    
+    return 0;
+  }
